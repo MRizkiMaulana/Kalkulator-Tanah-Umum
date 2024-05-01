3eb40abe5d04d32bbac8d261fb8d546aabbbfab2
MRizkiMaulana <rizkymaulana20799@gmail.com>
2024-05-02 05:17:26 +0700

    HitungManual

--- src/js/hargajual.js
+++ src/js/hargajual.js
@@ -1,4 +1,6 @@
-document.getElementById("nilaiPerolehanTanahForm").addEventListener("submit", function (event) {
+document
+  .getElementById("nilaiPerolehanTanahForm")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
 
     var namapenjual = document.getElementById("namapenjual").value;
@@ -6,69 +8,79 @@ document.getElementById("nilaiPerolehanTanahForm").addEventListener("submit", fu
     var alamat = document.getElementById("alamat").value;
     var alamattanah = document.getElementById("alamattanah").value;
     var nonopsppt = document.getElementById("nonopsppt").value;
-    var hitungType = document.querySelector('input[name="hitungType"]:checked').value;
+    var hitungType = document.querySelector(
+      'input[name="hitungType"]:checked'
+    ).value;
 
     // Mendefinisikan variabel numerik lainnya
-    var njop = parseFloat(document.getElementById("njop").value);
+    var hargaJual = parseFloat(document.getElementById("hargaJual").value);
     var luasTanah = parseFloat(document.getElementById("luasTanah").value);
-    var luasBangunan = parseFloat(document.getElementById("luasBangunan").value);
-    var njopBangunan = parseFloat(document.getElementById("njopBangunan").value);
 
-    // Menghitung NJOP persen
-    var njopPersen = njop * 0.25;
-
-    // Menghitung harga jual
-    var hargaJual = njop + njopPersen;
 
     // Menghitung total harga jual
     var totalHargaJual = hargaJual * luasTanah;
 
-    // Menghitung harga bangunan
-    var hargaBangunan = njopBangunan * luasBangunan;
-
-    // Menghitung nilai perolehan tanah
-    var nilaiPerolehanTanah = totalHargaJual + hargaBangunan;
 
     // Mendeklarasikan variabel untuk hasil perhitungan
-    var bphtb, pph, kasDesa, biayaNotaris, admin, rtrw, validasi, paralegal, sppt, segelJualBeli, nilaiPerkiraanHarga;
+    var bphtb,
+      pph,
+      kasDesa,
+      biayaNotaris,
+      admin,
+      rtrw,
+      validasi,
+      paralegal,
+      sppt,
+      segelJualBeli,
+      nilaiPerkiraanHarga;
 
     // Menghitung berdasarkan pilihan hitung
     if (hitungType === "hitung-ajb") {
-        // Menghitung bphtb
-        bphtb = nilaiPerolehanTanah >= 80000000 ? (nilaiPerolehanTanah - 80000000) * 0.05 : 0;
-        // Menghitung pph
-        pph = nilaiPerolehanTanah * 0.025;
-        // Menghitung kasdesa
-        kasDesa = nilaiPerolehanTanah * 0.05;
-        // Menghitung biaya notaris
-        biayaNotaris = nilaiPerolehanTanah * 0.03;
-        // Menghitung biaya admin
-        admin = 300000;
-        // Menghitung biaya RT/RW
-        rtrw = 500000;
-        // Menghitung biaya validasi
-        validasi = 250000;
-        // Menghitung biaya paralegal
-        paralegal = 300000;
-        // Menghitung biaya SPPT
-        sppt = 750000;
-        // Menghitung nilai perkiraan harga
-        nilaiPerkiraanHarga = bphtb + pph + kasDesa + biayaNotaris + admin + rtrw + validasi + paralegal + sppt;
+      // Menghitung bphtb
+      bphtb =
+        totalHargaJual >= 80000000 ? (totalHargaJual - 80000000) * 0.05 : 0;
+      // Menghitung pph
+      pph = totalHargaJual * 0.025;
+      // Menghitung kasdesa
+      kasDesa = totalHargaJual * 0.05;
+      // Menghitung biaya notaris
+      biayaNotaris = totalHargaJual * 0.03;
+      // Menghitung biaya admin
+      admin = 300000;
+      // Menghitung biaya RT/RW
+      rtrw = 500000;
+      // Menghitung biaya validasi
+      validasi = 250000;
+      // Menghitung biaya paralegal
+      paralegal = 300000;
+      // Menghitung biaya SPPT
+      sppt = 750000;
+      // Menghitung nilai perkiraan harga
+      nilaiPerkiraanHarga =
+        bphtb +
+        pph +
+        kasDesa +
+        biayaNotaris +
+        admin +
+        rtrw +
+        validasi +
+        paralegal +
+        sppt;
     } else if (hitungType === "hitung-segel") {
-        // Menghitung kasdesa
-        kasDesa = nilaiPerolehanTanah * 0.05;
-        // Menghitung biaya admin
-        admin = 300000;
-        // Menghitung biaya RT/RW
-        rtrw = 500000;
-        // Menghitung biaya validasi
-        validasi = 250000;
-        // Menghitung biaya paralegal
-        paralegal = 300000;
-        // Menghitung biaya SPPT
-        sppt = 750000;
-        // Menghitung segeljualbeli
-        segelJualBeli = kasDesa + admin + rtrw + validasi + paralegal + sppt;
+      // Menghitung kasdesa
+      kasDesa = totalHargaJual * 0.05;
+      // Menghitung biaya admin
+      admin = 300000;
+      // Menghitung biaya RT/RW
+      rtrw = 500000;
+      // Menghitung biaya validasi
+      validasi = 250000;
+      // Menghitung biaya paralegal
+      paralegal = 300000;
+      // Menghitung biaya SPPT
+      sppt = 750000;
+      // Menghitung segeljualbeli
+      segelJualBeli = kasDesa + admin + rtrw + validasi + paralegal + sppt;
     }
 
     // Menampilkan hasil sesuai pilihan hitung
@@ -81,15 +93,22 @@ document.getElementById("nilaiPerolehanTanahForm").addEventListener("submit", fu
         <p>Alamat Tanah: ${alamattanah}</p>
         <p>Nomor NOP SPPT: ${nonopsppt}</p>
         <p>Luas Tanah: ${luasTanah} m^2</p>
-        <p>Luas Bangunan: ${luasBangunan} m^2</p>
-        <p>Nilai Perolehan Tanah: ${nilaiPerolehanTanah.toLocaleString(
-          "id-ID",
-          { style: "currency", currency: "IDR" }
-        )}</p>
+        <p>Harga Jual: ${hargaJual.toLocaleString("id-ID", {
+          style: "currency",
+          currency: "IDR",
+        })}</p>
+        <p>Total Harga Jual: ${totalHargaJual.toLocaleString("id-ID", {
+          style: "currency",
+          currency: "IDR",
+        })}</p>
+        <p>Nilai Perolehan Tanah: ${totalHargaJual.toLocaleString("id-ID", {
+          style: "currency",
+          currency: "IDR",
+        })}</p>
         `;
 
     if (hitungType === "hitung-ajb") {
-        hasilElem.innerHTML += `
+      hasilElem.innerHTML += `
             <h3>Uraian :</h3>
             <p>BPHTB: ${bphtb.toLocaleString("id-ID", {
               style: "currency",
@@ -134,7 +153,7 @@ document.getElementById("nilaiPerolehanTanahForm").addEventListener("submit", fu
             )}</p>
             `;
     } else if (hitungType === "hitung-segel") {
-        hasilElem.innerHTML += `
+      hasilElem.innerHTML += `
             <h3>Uraian :</h3>
             <p>Kas Desa: ${kasDesa.toLocaleString("id-ID", {
               style: "currency",
@@ -167,4 +186,4 @@ document.getElementById("nilaiPerolehanTanahForm").addEventListener("submit", fu
             )}</p>
             `;
     }
-});
+  });
