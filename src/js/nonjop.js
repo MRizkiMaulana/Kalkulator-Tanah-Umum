12212ab0ebee3f3927dfdf6bf7e63e10721f4faf
MRizkiMaulana <rizkymaulana20799@gmail.com>
2024-05-02 06:47:44 +0700

    jarak

--- src/js/nonjop.js
+++ src/js/nonjop.js
@@ -86,7 +86,7 @@ document
     // Menampilkan hasil sesuai pilihan hitung
     var hasilElem = document.getElementById("hasil");
     hasilElem.innerHTML = `
-        <br><h2>Rencana Anggaran Biaya Pembuatan Surat Tanah</h2><br> 
+        <br><br><br><h2>Rencana Anggaran Biaya Pembuatan Surat Tanah</h2><br> 
         <p>Nama Penjual: ${namapenjual}</p>
         <p>Nama Pembeli: ${namapembeli}</p>
         <p>Alamat: ${alamat}</p>
